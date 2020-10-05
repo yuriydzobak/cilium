866fa902bd3bb49d46c241d823865a11c1161b02
Robin Hahling <robin.hahling@gw-computing.net>
2020-10-05 14:57:33 +0200

    image/hubble-proto: bump deps for cmd/protoc-gen-go-grpc/v1.0.1
    See release notes:
    https://github.com/grpc/grpc-go/releases/tag/cmd%2Fprotoc-gen-go-grpc%2Fv1.0.1
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- images/hubble-proto/install-protoplugins.sh
+++ images/hubble-proto/install-protoplugins.sh
@@ -14,12 +14,13 @@ go mod init github.com/cilium/hubble/protoc
 
 # latest tag at the time. For some reason `go get foo/bar/baz@vX.Y.Z` doesn't
 # work with nested go.mod definitions.
-go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@cee815d
+# a80e650 == tag: cmd/protoc-gen-go-grpc/v1.0.1
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@a80e65018272ac6bd58ee44d0d3e37374b090c1f
 go build google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 # protoc-gen-go-json doesn't have releases, this is the latest commit at the time
-go get github.com/mitchellh/protoc-gen-go-json@8fbb6f3
+go get github.com/mitchellh/protoc-gen-go-json@364b693
 go build github.com/mitchellh/protoc-gen-go-json
 
-go get github.com/golang/protobuf@v1.4.2
+go get github.com/golang/protobuf@v1.4.3
 go build github.com/golang/protobuf/protoc-gen-go
